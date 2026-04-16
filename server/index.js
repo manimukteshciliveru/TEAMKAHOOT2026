b31aae38d8a8fe28588ef1da64951b936778baef
samanvichidambaram-wq <samanvi.chidambaram@gmail.com>
2026-04-17 00:00:11 +0530

    Fixed missing path import and finalized cloud stability

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path'); // MISSING IMPORT FIXED
 const connectDB = require('./config/db');
 
 const app = express();
