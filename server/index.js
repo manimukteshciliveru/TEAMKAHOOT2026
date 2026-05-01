40745aaaf20e00460332337f5ce86cdb3d1bb28b
manimukteshciliveru_1218 <manimukteshciliveru@gmail.com>
2026-05-01 21:57:39 +0530

    Fix: restored missing routes in index.js

--- server/index.js
+++ server/index.js
@@ -50,6 +50,10 @@ app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/quiz', require('./routes/quiz'));
+
 // Models
 const User = require('./models/User');
 const Quiz = require('./models/Quiz');
